badbc51067294dbd6d209155f5b69ed4ddd069c4
Daniel BLAHOVEC <dan792007@gmail.com>
2026-05-24 19:51:45 +0200

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'price',
+        'description',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'price' => 'decimal:2',
+        ];
+    }
+
+    public function images(): HasMany
+    {
+        return $this->hasMany(ProductImage::class);
+    }
+
+    public function categories(): BelongsToMany{
+        return $this->belongsToMany(Category::class);
+    }
+
+
+}
---
+++ denik.md
@@ -0,0 +1,7 @@
+<h1>Laravel</h1>
+<h2>Databáze</h2>
+<ol>
+<li>Design databáze</li>
+<li>Vytvoření migrací</li>
+</ol>
+
